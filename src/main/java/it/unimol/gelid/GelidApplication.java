73a7da06684626ca8a4a9f9c7e162c5121b24c9e
azezeB <azezebrajov@gmail.com>
2023-01-24 12:55:31 +0100

    Initial commit

---
+++ src/main/java/it/unimol/gelid/GelidApplication.java
@@ -0,0 +1,13 @@
+package it.unimol.gelid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimol/gelid/GelidApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimol.gelid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
